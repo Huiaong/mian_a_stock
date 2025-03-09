aeaad0dd81ed6585a30e4ea7bf130e245780a2cc
v2seeu <v2seeu@gmail.com>
2025-03-09 23:40:23 +0800

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,33 @@
 module.exports = {
   root: true,
   globals: {
-    chrome: true,
+    chrome: true
   },
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
-    "generator-star-spacing": "off",
-    "object-curly-spacing": "off",
-    "no-var": "error",
-    "semi": 0,
-    "eol-last": "off",
-    "no-tabs": "off",
-    "indent": "off",
-    "quote-props": 0,
-    "no-mixed-spaces-and-tabs": "off",
-    "no-trailing-spaces": "off",
-    "arrow-parens": 0,
-    "spaced-comment": "off",
-    "space-before-function-paren": "off",
-    "no-empty": "off",
-    "no-else-return": "off",
-    "no-unused-vars": [2, {"vars": "all", "args": "after-used"}],
-    "no-console": "off",
+    'generator-star-spacing': 'off',
+    'object-curly-spacing': 'off',
+    'no-var': 'error',
+    semi: 0,
+    'eol-last': 'off',
+    'no-tabs': 'off',
+    indent: 'off',
+    'quote-props': 0,
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-trailing-spaces': 'off',
+    'arrow-parens': 0,
+    'spaced-comment': 'off',
+    'space-before-function-paren': 'off',
+    'no-empty': 'off',
+    'no-else-return': 'off',
+    'no-unused-vars': [2, { vars: 'all', args: 'after-used' }],
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
